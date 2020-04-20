30d1d59e34a3d023610f0670184d37296b16a4f5
fengming.jiang <fengming.jiang@bjyada.com>
2020-04-20 10:23:25 +0800

    验证码接口注释补全

--- gw/src/main/kotlin/com/yada/services/PwdDigestService.kt
+++ gw/src/main/kotlin/com/yada/services/PwdDigestService.kt
@@ -6,11 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
+/**
+ * 密码接口
+ */
 interface IPwdDigestService {
+    /**
+     * 获取默认的密码摘要
+     * @param username 用户名
+     */
     fun getDefaultPwdDigest(username: String): String
+
+    /**
+     * 根据密码获取密码摘要
+     * @param username 用户名
+     * @param pwdPlaintext 密码明文
+     */
     fun getPwdDigest(username: String, pwdPlaintext: String): String
 }
 
+// TODO 密码安全性不符合要求
 @Service
 class PwdDigestService @Autowired constructor(@Value("\${yada.user.defaultPwd:changepwd}") private val defaultPwd: String) : IPwdDigestService {
     override fun getDefaultPwdDigest(username: String): String = getPwdDigest(username, defaultPwd)
