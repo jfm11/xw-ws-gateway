937407036d6dafa6b4e8155a30fb64046f689e39
fengming.jiang <fengming.jiang@bjyada.com>
2020-04-20 10:19:24 +0800

    验证码接口注释补全

--- gw/src/main/kotlin/com/yada/services/RecaptchaService.kt
+++ gw/src/main/kotlin/com/yada/services/RecaptchaService.kt
@@ -5,11 +5,25 @@ import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
 
+/**
+ * 验证码服务
+ */
 interface IRecaptchaService {
+    /**
+     * 检查验证码
+     */
     fun check(code: String): Mono<Boolean>
+
+    /**
+     * 从数据中获取验证码
+     */
     fun getCode(data: Map<String, String>): String?
 }
 
+/**
+ * 谷歌验证服务
+ * @param url 验证服务地址
+ */
 abstract class AbsGoogleRecaptchaService(private val url: String) : IRecaptchaService {
     override fun check(code: String): Mono<Boolean> {
         return WebClient
@@ -35,6 +49,9 @@ open class GoogleRecaptchaService : AbsGoogleRecaptchaService("https://www.googl
 @Service("recaptcha.GoogleCN")
 open class GoogleCnRecaptchaService : AbsGoogleRecaptchaService("https://recaptcha.net/recaptcha/api/siteverify?secret=6Leu2NsUAAAAAKbXdSjLz6_c1cf8qX2bL4xfn1mN&response=")
 
+/**
+ * 不使用验证码
+ */
 @Service("recaptcha.None")
 open class NoneRecaptchaService : IRecaptchaService {
     override fun check(code: String): Mono<Boolean> = Mono.just(true)
