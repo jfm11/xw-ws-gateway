1033848135f67c6c00ef41d1735e3e5f8cc774a9
fengming.jiang <fengming.jiang@bjyada.com>
2020-04-20 09:47:43 +0800

    角色接口注释补全

--- gw/src/main/kotlin/com/yada/services/RoleService.kt
+++ gw/src/main/kotlin/com/yada/services/RoleService.kt
@@ -8,11 +8,33 @@ import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
+/**
+ * 角色接口
+ */
 interface IRoleService {
+    /**
+     * 获取所有角色
+     */
     fun getAll(): Flux<Role>
+
+    /**
+     * 根据ID获取角色
+     */
     fun get(id: String): Mono<Role>
+
+    /**
+     * 根据ID判断角色是否存在
+     */
     fun exist(id: String): Mono<Boolean>
+
+    /**
+     * 创建或更新角色
+     */
     fun createOrUpdate(role: Role): Mono<Role>
+
+    /**
+     * 根据ID删除角色
+     */
     fun delete(id: String): Mono<Void>
 }
 
@@ -30,8 +52,12 @@ open class RoleService @Autowired constructor(private val roleRepo: RoleReposito
 
     @Transactional
     override fun delete(id: String): Mono<Void> = userService.getAll()
+            // 找到存在当前角色用户
             .filter { user -> id in user.roles }
+            // 删除当前用户的该角色
             .map { user -> user.copy(roles = user.roles - id) }
+            // 保存或更新用户
             .flatMap(userService::createOrUpdate)
+            // 删除角色
             .then(roleRepo.deleteById(id))
 }
